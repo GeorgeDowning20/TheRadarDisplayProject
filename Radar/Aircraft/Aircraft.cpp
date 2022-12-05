f52f4e673bcd2f60efb9abb4cf003f0578a499f6
George Downing <george.downing20@icloud.com>
2022-12-05 14:15:57 +0000

    final

--- Radar/Aircraft/Aircraft.cpp
+++ Radar/Aircraft/Aircraft.cpp
@@ -1,16 +1,37 @@
-#include "Aircraft.h"
+//---------------------------------------------------------------------------//
+/// @file Aircraft.cpp
+/// @brief This file contains the Airspace struct Definitions
+/// @author G.Downing
+/// @date 2020-11-24
+/// @copyright (c) 2022 G. Downing
+/// @details This file contains the Aircraft struct Definitions. The Aircraft struct is responsible for storing the aircraft characteristic data for each type of aircraft, including icon and movement data.
+/// @details Doxygen Documentation for project: https://georgedowning20.github.io/TheRadarDisplayProject/index.html
+//---------------------------------------------------------------------------//
 
+//---------------------------------------------------------------------------//
+//                        preprocessor directives
+//---------------------------------------------------------------------------//
+
+#include "Aircraft.h" // Aircraft structures
+
+//---------------------------------------------------------------------------//
+//                           struct Definitions
+//---------------------------------------------------------------------------//
+
+/// @details This function returns the bounding box for the aircraft
 Rect Aircraft::bounds() const
 {
-    return {position.x, position.y, size.width, size.height};
+    return {position.x, position.y, size.width, size.height}; // Return the bounding box for the aircraft
 }
 
+/// @details This function is the constructor for the jet struct. The icon is set to the jet icon, and the last update time is set to the current time to initialise the movement.
 Jet_s::Jet_s() : Aircraft(0x3368ff, {100, 100}, {3, 3})
 {
-    icon_.set_Icon(1, JetIcon);
-    time_at_last_move_ns_ = time_.time();
+    icon_.set_Icon(1, JetIcon);           // Set the icon to the jet icon
+    time_at_last_move_ns_ = time_.time(); // Set the last update time to the current time
 };
 
+/// @details This function defines the movement characteristics for the jet. The jet moves randomly at a the defined speed.
 void Jet_s::move()
 {
     int direction = std::rand() % 4;
@@ -35,52 +56,20 @@ void Jet_s::move()
     time_at_last_move_ns_ = time_.time();
 }
 
+/// @details This functions polls the time since the last update, and if the time is greater than the defined update time, the aircraft moves. This regulates the update rate of the aircraft.
 bool Jet_s::is_Time_To_Move()
 {
     return ((time_.time() - time_at_last_move_ns_) >= move_time_ns_);
 }
 
-Heli_s::Heli_s() : Aircraft(0xff0000, {100, 100}, {3, 3})
-{
-    icon_.set_Icon(1, HeliIcon);
-    time_at_last_move_ns_ = time_.time();
-};
-
-void Heli_s::move()
-{
-    int direction = std::rand() % 4;
-    const int MOVE_DIST = 5;
-
-    switch (direction)
-    {
-    case 0:
-        position.y -= MOVE_DIST;
-        break;
-    case 1:
-        position.y += MOVE_DIST;
-        break;
-    case 2:
-        position.x -= MOVE_DIST;
-        break;
-    case 3:
-        position.x += MOVE_DIST;
-        break;
-    }
-
-    time_at_last_move_ns_ = time_.time();
-}
-
-bool Heli_s::is_Time_To_Move()
-{
-    return ((time_.time() - time_at_last_move_ns_) >= move_time_ns_);
-}
-
+/// @details This function is the constructor for the plane struct. The icon is set to the plane icon, and the last update time is set to the current time to initialise the movement.
 Plane_s::Plane_s() : Aircraft(0x33ff4c, {100, 100}, {4, 4})
 {
     icon_.set_Icon(1, PlaneIcon);
     time_at_last_move_ns_ = time_.time();
 };
 
+/// @details This function defines the movement characteristics for the plane. The plane moves randomly at a the defined speed.
 void Plane_s::move()
 {
     int direction = std::rand() % 4;
@@ -105,7 +94,46 @@ void Plane_s::move()
     time_at_last_move_ns_ = time_.time();
 }
 
+/// @details This functions polls the time since the last update, and if the time is greater than the defined update time, the aircraft moves. This regulates the update rate of the aircraft.
 bool Plane_s::is_Time_To_Move()
+{
+    return ((time_.time() - time_at_last_move_ns_) >= move_time_ns_);
+}
+
+/// @details This function is the constructor for the helicopter struct. The icon is set to the helicopter icon, and the last update time is set to the current time to initialise the movement.
+Heli_s::Heli_s() : Aircraft(0xff0000, {100, 100}, {3, 3})
+{
+    icon_.set_Icon(1, HeliIcon);
+    time_at_last_move_ns_ = time_.time();
+};
+
+/// @details This function defines the movement characteristics for the helicopter. The helicopter moves randomly at a the defined speed.
+void Heli_s::move()
+{
+    int direction = std::rand() % 4;
+    const int MOVE_DIST = 5;
+
+    switch (direction)
+    {
+    case 0:
+        position.y -= MOVE_DIST;
+        break;
+    case 1:
+        position.y += MOVE_DIST;
+        break;
+    case 2:
+        position.x -= MOVE_DIST;
+        break;
+    case 3:
+        position.x += MOVE_DIST;
+        break;
+    }
+
+    time_at_last_move_ns_ = time_.time();
+}
+
+/// @details This functions polls the time since the last update, and if the time is greater than the defined update time, the aircraft moves. This regulates the update rate of the aircraft.
+bool Heli_s::is_Time_To_Move()
 {
     return ((time_.time() - time_at_last_move_ns_) >= move_time_ns_);
 }
--- Radar/Aircraft/Aircraft.h
+++ Radar/Aircraft/Aircraft.h
@@ -1,62 +1,110 @@
-#pragma once
+//---------------------------------------------------------------------------//
+/// @file Aircraft.h
+/// @brief This file contains the Airspace struct Deceleration
+/// @author G.Downing
+/// @date 2020-11-24
+/// @copyright (c) 2022 G. Downing
+/// @details This file contains the Aircraft struct Deceleration. The Aircraft struct is responsible for storing the aircraft characteristic data for each type of aircraft, including icon and movement data.
+/// @details Doxygen Documentation for project: https://georgedowning20.github.io/TheRadarDisplayProject/index.html
+//---------------------------------------------------------------------------//
 
-#include "../def.h"
-#include "../Time/Time.h"
-#include "../Icon/Icon.h"
+//---------------------------------------------------------------------------//
+//                        preprocessor directives
+//---------------------------------------------------------------------------//
 
-#include "../Icon/Jet.inc"
-#include "../Icon/Plane.inc"
-#include "../Icon/Heli.inc"
+#pragma once // Include guard
 
-// define a set of 16 pixels to represent a jet
+#include "../def.h"       // def.h
+#include "../Time/Time.h" // Time class
+#include "../Icon/Icon.h" // Icon class
+
+#include "../Icon/Jet.inc"   // Jet icon
+#include "../Icon/Plane.inc" // Plane icon
+#include "../Icon/Heli.inc"  // Helicopter icon
+
+//---------------------------------------------------------------------------//
+//                           struct Deceleration
+//---------------------------------------------------------------------------//
+
+/// @brief Aircraft structure
+/// @details This is the base struct for an aircraft. The Aircraft struct is responsible for storing the aircraft characteristic data for each type of aircraft.
+/// @date 2020-11-24
+/// @author G.Downing
 struct Aircraft
 {
-    unsigned long color = 0x6091ab;
-    Point position{10, 10};
-    Size size{3, 3};
-    icon icon_;
+    unsigned long color = 0x6091ab; ///< The unsigned long instance used to store the aircraft color
+    Point position{10, 10};         ///< The Point instance used to store the aircraft position
+    Size size{3, 3};                ///< The Size instance used to store the aircraft size
+    icon icon_;                     ///< The icon instance used to store the aircraft icon
 
+    /// @brief constructor the aircraft struct
+    /// @param new_col the new aircraft color
+    /// @param new_pos the new aircraft position
+    /// @param new_sz   the new aircraft size
     Aircraft(unsigned long new_col, Point new_pos, Size new_sz)
         : color(new_col), position(new_pos), size(new_sz){};
 
+    /// @brief return the bounding box for the aircraft
+    /// @return the bounding box for the aircraft
     Rect bounds() const;
 };
 
+/// @brief Jet structure
+/// @details This is the Jet struct. The Jet struct is responsible for storing the aircraft characteristic data for a jet.
+/// @note This struct is derived from the Aircraft struct.
 struct Jet_s : public Aircraft
 {
+    /// @brief constructor the jet struct
     Jet_s();
 
+    /// @brief define movement characteristics for the jet
     void move();
 
+    /// @brief define condition for updating the jet
+    /// @return true if the jet should be updated
     bool is_Time_To_Move();
 
-    long time_at_last_move_ns_;
-    long move_time_ns_{250'000'000};
-    _Time time_;
+    long time_at_last_move_ns_;      ///< The long instance used to store the time at last move in nanoseconds
+    long move_time_ns_{250'000'000}; ///< The long instance used to store the move time in nanoseconds
+    _Time time_;                     ///< The _Time instance used to store the time
 };
 
+/// @brief Heli structure
+/// @details This is the Heli struct. The Heli struct is responsible for storing the aircraft characteristic data for a Heli.
+/// @note This struct is derived from the Aircraft struct.
 struct Heli_s : public Aircraft
 {
+    /// @brief constructor the heli struct
     Heli_s();
 
+    /// @brief define movement characteristics for the heli
     void move();
 
+    /// @brief define condition for updating the heli
+    /// @return true if the heli should be updated
     bool is_Time_To_Move();
 
-    long time_at_last_move_ns_;
-    long move_time_ns_{500'000'000};
-    _Time time_;
+    long time_at_last_move_ns_;      ///< The long instance used to store the time at last move in nanoseconds
+    long move_time_ns_{500'000'000}; ///< The long instance used to store the move time in nanoseconds
+    _Time time_;                     ///< The _Time instance used to store the time
 };
 
+/// @brief plane structure
+/// @details This is the plane struct. The plane struct is responsible for storing the aircraft characteristic data for a plane.
+/// @note This struct is derived from the Aircraft struct.
 struct Plane_s : public Aircraft
 {
+    /// @brief constructor the plane struct
     Plane_s();
 
+    /// @brief define movement characteristics for the plane
     void move();
 
+    /// @brief define condition for updating the plane
+    /// @return true if the plane should be updated
     bool is_Time_To_Move();
 
-    long time_at_last_move_ns_;
-    long move_time_ns_{800'000'000};
-    _Time time_;
+    long time_at_last_move_ns_;      ///< The long instance used to store the time at last move in nanoseconds
+    long move_time_ns_{800'000'000}; ///< The long instance used to store the move time in nanoseconds
+    _Time time_;                     ///< The _Time instance used to store the time
 };
