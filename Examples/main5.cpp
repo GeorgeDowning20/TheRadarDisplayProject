5212044168e3b31973adc771f788fdbf62bac108
George Downing <george.downing20@icloud.com>
2022-12-05 02:12:32 +0000

    .

--- Examples/main5.cpp
+++ Examples/main5.cpp
@@ -22,24 +22,25 @@
 /// @brief main program
 /// @return 0
 /// @details This is the main function for the program. an icon is created named myIcon. the icon is then initialised as a diagonal line using the #initialiseAsDefaultDiagonalLine(uint8_t Brightness) function from the Icon class. the icon is then printed to the console using the #showIconDetail function from the Icon class.
-/**  @test $Output:\n
-Icon 0 has the following pixels:\n
-Pixel at (0, 0) has a brightness of 15\n
-Pixel at (1, 1) has a brightness of 15\n
-Pixel at (2, 2) has a brightness of 15\n
-Pixel at (3, 3) has a brightness of 15\n
-Pixel at (4, 4) has a brightness of 15\n
-Pixel at (5, 5) has a brightness of 15\n
-Pixel at (6, 6) has a brightness of 15\n
-Pixel at (7, 7) has a brightness of 15\n
-Pixel at (8, 8) has a brightness of 15\n
-Pixel at (9, 9) has a brightness of 15\n
-Pixel at (10, 10) has a brightness of 15\n
-Pixel at (11, 11) has a brightness of 15\n
-Pixel at (12, 12) has a brightness of 15\n
-Pixel at (13, 13) has a brightness of 15\n
-Pixel at (14, 14) has a brightness of 15\n
-Pixel at (15, 15) has a brightness of 15$
+/**  @test
+Output: \n
+Icon 0 has the following pixels: \n
+Pixel at (0, 0) has a brightness of 15 \n
+Pixel at (1, 1) has a brightness of 15 \n
+Pixel at (2, 2) has a brightness of 15 \n
+Pixel at (3, 3) has a brightness of 15 \n
+Pixel at (4, 4) has a brightness of 15 \n
+Pixel at (5, 5) has a brightness of 15 \n
+Pixel at (6, 6) has a brightness of 15 \n
+Pixel at (7, 7) has a brightness of 15 \n
+Pixel at (8, 8) has a brightness of 15 \n
+Pixel at (9, 9) has a brightness of 15 \n
+Pixel at (10, 10) has a brightness of 15 \n
+Pixel at (11, 11) has a brightness of 15 \n
+Pixel at (12, 12) has a brightness of 15 \n
+Pixel at (13, 13) has a brightness of 15 \n
+Pixel at (14, 14) has a brightness of 15 \n
+Pixel at (15, 15) has a brightness of 15
 **/
 /// @date 2020-11-24
 /// @author G.Downing
